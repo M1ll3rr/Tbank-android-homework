1582bf9088f1ec3f9e5a9a63176602af45514099
M1ll3rr <tmaks232003@gmail.com>
2025-05-05 20:13:21 +0300

    11 homework

--- app/src/main/java/com/example/myfirstapp/shops/AbstractShop.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myfirstapp.shops
-
-import com.example.myfirstapp.library.LibraryItem
-
-abstract class AbstractShop<T : LibraryItem> : Shop<T> {
-    override fun sell(choice: Int): T {
-        return itemList.removeAt(choice)
-    }
-}
